b591600bdb8998c5a85edcb5d6da572ebbf694ec
NunexGT <74361788+NunexGTX@users.noreply.github.com>
2025-12-06 17:05:33 +0000

    dynamic camera test course with triggers

--- Scripts/CameraWorks/Camera.gd
+++ Scripts/CameraWorks/Camera.gd
@@ -1,7 +1,8 @@
 extends Camera3D
 
 @export var spring_arm: Node3D
-@export var lerp_power: float = 1.0
+@export var movement_lerp_power: float = 1.0
+@export var focalLenght_lerp_power: float = 0.95
 
 #aim offsets
 @export var cameraAimOfset: Vector3 = Vector3(1,0,0)
@@ -22,16 +23,16 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	var cameraAim = __aim(delta)
-	position = lerp(position, spring_arm.position + cameraAim,delta*lerp_power)
+	position = lerp(position, spring_arm.position + cameraAim,delta*movement_lerp_power)
 	
 #aim input
 func __aim(delta: float) -> Vector3:
 	#aim
 	if Input.is_action_pressed("aim"):
-		$".".fov = lerp($".".fov,get_aimFov(),delta*lerp_power)
+		$".".fov = lerp($".".fov,get_aimFov(),delta*focalLenght_lerp_power)
 		return cameraAimOfset
 	else:
-		$".".fov = lerp($".".fov,get_cameraFOV(),delta*lerp_power)
+		$".".fov = lerp($".".fov,get_cameraFOV(),delta*focalLenght_lerp_power)
 		return Vector3.ZERO
 		
 
@@ -43,4 +44,7 @@ func get_aimFov() -> float:
 	return rad_to_deg(2* atan(VIRTUAL_SENSOR_SIZE_MM/(2*aimFocalLenght)))
 	
 func set_camera_FocalLenght(new_focal_lenght: float) -> void:
+	if new_focal_lenght <= 0:
+		FocalLenght = cameraFocalLenght
+		return
 	FocalLenght = new_focal_lenght
